6e7a958d856eb8d9c66a90dd110d58552a85183d
Rail <rail@MacBook-Air-Rail.local>
2023-03-02 12:21:27 +0500

    Initial commit

---
+++ src/main/java/ru/rail/springcourse/models/Person.java
@@ -0,0 +1,66 @@
+package ru.rail.springcourse.models;
+
+
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class Person {
+    private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
+    private String name;
+
+    @NotEmpty(message = "Surname should not be empty")
+    @Size(min = 2, max = 30, message = "Surname should be between 2 and 30 characters")
+    private String surname;
+
+    @NotEmpty(message = "Patronymic should not be empty")
+    @Size(min = 2, max = 30, message = "Patronymic should be between 2 and 30 characters")
+    private String patronymic;
+
+    public Person() {
+
+    }
+    public Person(int id, String name, String surname, String patronymic) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.patronymic = patronymic;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+}
